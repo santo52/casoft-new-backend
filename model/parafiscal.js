bc9f6084904274cae3575287afa3e9326f4704fe
Santiago Ruiz <sanruiz1003@gmail.com>
2020-02-04 06:43:37 -0500

    nuevos campos parafiscales

--- model/parafiscal.js
+++ model/parafiscal.js
@@ -7,6 +7,15 @@ const Name = 'Parafiscal'
 const Schema = new mongoose.Schema({
   name: { type: String, required: true },
   isDiscount: { type: Boolean, default: false },
+  employeePercentage: { type: Number, required: true },
+  companyPercentage: { type: Number, required: true },
+  from: { type: String, required: true }, // Porcentajes calculados a partir del salario minimo, ingreso base cotización o salario
+  chronology: { type: String, required: true },
+  withSalaryRange: { type: Boolean, default: false },
+  salaryRange: { //rango de salarios mínimos al que aplica
+    from: String,
+    to: String
+  },
   deleted: { type: Boolean, default: false }
 })
 
